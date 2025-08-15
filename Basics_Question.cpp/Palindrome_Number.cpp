7ed94769b22273345b4bd53de2ad2b4cb49bdce4
VineetKumar2004 <vineet6645@gmail.com>
2025-08-15 21:45:48 +0530

    palindrome number solution

---
+++ Basics_Question.cpp/Palindrome_Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int rem,ans=0,num=x;
+        if(x<0)
+        return 0;
+        while(num)
+        {
+            rem=num%10;
+            num/=10;
+            if(ans>INT_MAX/10)
+            return 0;
+            ans=ans*10+rem;
+        }
+        if(ans==x)
+        return 1;
+        else
+        return 0;
+    }
+};
