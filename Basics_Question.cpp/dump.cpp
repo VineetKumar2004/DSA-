0c785935d9e420d16ac0aec9d844c2fd738fd74a
Vineet Kumar <vineet6645@gmail.com>
2026-05-02 15:04:19 +0530

    factorial of no. with recursion

--- Basics_Question.cpp/dump.cpp
+++ Basics_Question.cpp/dump.cpp
@@ -49,30 +49,73 @@
 // }
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Solution{
+//     public:
+//     int print(int n,int p){
+//         if(n==p){
+//             cout<<p;
+//             return;
+//         }
+//         cout<<n<<",";
+//         print(n+1,p);
+//     }
+// };
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int p;
+//     cin>>p;
+
+//     Solution obj;
+//     cout<<"[";
+//     cout<<obj.print(n,p);
+//     cout<<"]";
+//     return 0;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Solution{
+//     public:
+//     void printEven(int num,int n){
+//         if(num>n){
+//             return;
+//         }
+//         printEven(num+2,n);
+//         cout<<num<<",";
+        
+//     }
+// };
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     Solution obj;
+//     obj.printEven(2,n);
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution{
     public:
-    int print(int n,int p){
-        if(n==p){
-            cout<<p;
-            return;
-        }
-        cout<<n<<",";
-        print(n+1,p);
+    int fact(int n){
+        if(n == 1) return 1;
+    
+    return n*fact(n-1);
     }
 };
 
 int main(){
     int n;
     cin>>n;
-    int p;
-    cin>>p;
-
     Solution obj;
-    cout<<"[";
-    cout<<obj.print(n,p);
-    cout<<"]";
+    cout<<obj.fact(n);
     return 0;
 }
