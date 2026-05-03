0fe982286046bbc1922e3148bd72beba6bcf38c0
Vineet Kumar <vineet6645@gmail.com>
2026-05-03 23:51:48 +0530

    Solved Fibonachi series with recurision

---
+++ Basics_Question.cpp/fabnochi.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+       if(n==0) return 0;
+       if(n==1) return 1;
+
+        return fib(n-1)+fib(n-2);
+    }
+};
+
+int main(){
+    int n;
+    cin>>n;
+    Solution obj;
+    cout<<obj.fib(n);
+
+    return 0;
+}
