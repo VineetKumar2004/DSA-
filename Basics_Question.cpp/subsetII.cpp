d2f7f877c072417d5e6c928c5654c199d1f32656
Vineet Kumar <vineet6645@gmail.com>
2026-05-07 17:59:45 +0530

    Solved Subset II problem on the leetcode with the help of recurison

---
+++ Basics_Question.cpp/subsetII.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+     void subset(vector<int>& nums,int index,int n,vector<vector<int>>& ans, vector<int>& temp){
+        if(index == n){
+            ans.push_back(temp);
+            return;
+        }
+
+        //include
+        temp.push_back(nums[index]);
+        subset(nums,index+1,n,ans,temp);
+        temp.pop_back(); //backtrack
+
+        while(index+1 < n && nums[index] == nums[index+1]) index++;
+
+        //exclude
+        subset(nums,index+1,n,ans,temp);
+
+        
+     }
+};
+
+int main(){
+    vector<int>nums={1,2,2,3,3};
+    sort(nums.begin(), nums.end());
+    vector<vector<int>>ans;
+    vector<int>temp;
+    Solution obj;
+    obj.subset(nums,0,nums.size(),ans,temp);
+    for(int i = 0; i < ans.size(); i++) {
+    
+        for(int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
