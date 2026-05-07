5b8394d5f3f7584a264e6266566a4489a8886e21
Vineet Kumar <vineet6645@gmail.com>
2026-05-07 14:45:04 +0530

    Solved Subset problem on leetcode with the help of Recursion

---
+++ Basics_Question.cpp/PowerofFour.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+       if (n == 1) return true;
+        if (n <= 0 || n % 4 != 0) return false;
+        return isPowerOfThree(n3/ 3);
+    }
+};
