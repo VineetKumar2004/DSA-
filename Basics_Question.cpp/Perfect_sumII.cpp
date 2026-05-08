51d05be74d86d92c4fe668af6f544da7ac865782
Vineet Kumar <vineet6645@gmail.com>
2026-05-08 09:46:01 +0530

    Solved perfect sum where we are adding the element of array till its get equal to the target

---
+++ Basics_Question.cpp/Perfect_sumII.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+     bool perfectSum(int arr,int index,int n,int target){
+        if(target==0) return 1;
+        if(index==n || target<0) return 0;
+
+        return perfectSum(arr,index+1,n,target) || perfectSum(arr,index+1,n,target-arr[index]);
+     } 
+};
+
+int main() {
+
+    int arr[] = {1, 2, 6, 9};
+    int target = 10;
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bool ans = perfectSum(arr, 0, n, target);
+
+    if (ans)
+        cout << "Subset exists";
+    else
+        cout << "Subset does not exist";
+
+    return 0;
+}
