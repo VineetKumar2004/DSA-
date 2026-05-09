c9fa664f18dc87178c2a71f32d5cbcf95bc3980f
Vineet Kumar <vineet6645@gmail.com>
2026-05-09 23:51:08 +0530

    Solved Combination sum problem on leetcode with the help of recursion

--- Basics_Question.cpp/Perfect_sumII.cpp
+++ Basics_Question.cpp/Perfect_sumII.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Solution{
     public:
-     bool perfectSum(int arr,int index,int n,int target){
+     bool perfectSum(int arr[],int index,int n,int target){
         if(target==0) return 1;
         if(index==n || target<0) return 0;
 
@@ -17,8 +17,9 @@ int main() {
     int target = 10;
 
     int n = sizeof(arr) / sizeof(arr[0]);
+    Solution obj;
 
-    bool ans = perfectSum(arr, 0, n, target);
+    bool ans = obj.perfectSum(arr, 0, n, target);
 
     if (ans)
         cout << "Subset exists";
