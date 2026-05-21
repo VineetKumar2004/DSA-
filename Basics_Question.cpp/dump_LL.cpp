a090fe23bf71185fc3bc065614713a221334fb89
Vineet Kumar <vineet6645@gmail.com>
2026-05-21 13:11:33 +0530

    Practiced the Linked List problem

--- Basics_Question.cpp/dump_LL.cpp
+++ Basics_Question.cpp/dump_LL.cpp
@@ -10,10 +10,53 @@ class Node{
         next=NULL;
     }
 };
-
 int main(){
-    Node *Head;
-    Head=new Node(4);
-    cout<<Head->data<<endl;
-    cout<<Head->next;
+    // Node *Head;
+    // Head=new Node(4);
+    // cout<<Head->data<<endl;
+    // cout<<Head->next<<endl;
+
+    // int arr[]={1,4,7,6,9};
+    // for(int i=0;i<4;i++){
+    //     if(Head == NULL){
+    //         Head=new Node(arr[i]);
+    //     }
+    //     // LL EXISTS
+    //     else{
+    //         Node *temp;
+    //         temp = new Node(arr[i]);
+    //         temp->next=Head;
+    //         Head=temp;
+    //     }
+    // }
+
+    // //print the value
+    // Node *temp=Head;
+    // while(temp != NULL){
+    //     cout<<temp->data<<" ";
+    //     temp=temp->next;
+    // }
+
+   Node *Head, *Tail;
+   Head=Tail=NULL;
+
+   int arr[]={2,5,3,7,8};
+   for(int i=0;i<5;i++){
+    if(Head == NULL){
+        Head=new Node(arr[i]);
+        Tail = Head;
+    }
+
+    else{
+        Tail->next=new Node(arr[i]);
+        Tail=Tail->next;
+    }
+   }
+   Node *temp;
+   temp=Head;
+   while(temp != NULL){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+   }
+
 }
