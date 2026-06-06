e58af8c5caafc9ad4e7abc1b95368e3f8ae85351
Vineet Kumar <vineet6645@gmail.com>
2026-06-06 15:49:41 +0530

    Solved Detect Loop in Linked List II in leetcode

--- Basics_Question.cpp/Detect-Loop-in-LL.cpp
+++ Basics_Question.cpp/Detect-Loop-in-LL.cpp
@@ -1,1 +1,14 @@
-class Solution
+class Solution{
+    public:
+     bool detect(ListNode *head){
+         ListNode *fast=head;
+         ListNode *slow=head;
+
+         while(fast != NULL && fast->next != NULL){
+            slow=slow->next;
+            fast= fast->next->next;
+            if(slow == fast) return 1;
+         }
+         return 0;
+     }
+}
