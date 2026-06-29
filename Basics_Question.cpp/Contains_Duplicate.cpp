0f0685e7e5db3553b902d80ee8788659a840b736
Vineet Kumar <vineet6645@gmail.com>
2026-06-29 17:49:56 +0530

    Solved contains duplicate in the Leetcode

---
+++ Basics_Question.cpp/Contains_Duplicate.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    bool dup(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
+    }
+};
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    Solution obj;
+    cout<<obj.dup(nums);
+}
